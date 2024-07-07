1813f2959a8c405b244b866cfae266e7984a471e
Pavel <percheck298@gmail.com>
2024-07-07 16:23:35 +0300

    Added input of the first operand

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const BUTTON_TYPES = {
+    OPERATOR: 'operator',
+    OPERAND: 'operand',
+    EQUAL: 'equal',
+}
 const calculatorState = {
+    displayValue: '0', 
     firstOperand: null,
     operatorType: null,
     result: null,
@@ -12,3 +18,28 @@ function setOperator() {
 function calcResult() {
 
 }
+function showDisplayValue(value, display) {
+    display.value = value;
+}
+
+
+function main() {
+    const display = document.querySelector('.input');
+    const buttons = document.querySelector('.buttons');
+    buttons.addEventListener('click', function(event) {
+        const buttonType = event.target.dataset.buttonType;
+        if(buttonType === BUTTON_TYPES.OPERAND) {
+            if(calculatorState.displayValue === '0') {
+                calculatorState.displayValue = event.target.value;
+            } else {
+                calculatorState.displayValue += event.target.value;
+            }
+            showDisplayValue(calculatorState.displayValue, display);
+        }
+    })
+    window.onload = function() {
+        showDisplayValue(calculatorState.displayValue, display);
+    }
+
+}
+main();
