e6f1cd20f59fabb98a76af28a04c2bc247b97abc
Pavel <percheck298@gmail.com>
2024-07-07 14:45:11 +0300

    Added UI and form of state

---
+++ script.js
@@ -0,0 +1,14 @@
+const calculatorState = {
+    firstOperand: null,
+    operatorType: null,
+    result: null,
+};
+function setOperand() {
+
+}
+function setOperator() {
+
+}
+function calcResult() {
+
+}
